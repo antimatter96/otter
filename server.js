defaff89440362ff6bb6e1adfa805335622c27cf
Arpit Jain <antimatter96@users.noreply.github.com>
2017-02-16 20:47:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,22 +13,28 @@ var redis = require('redis');
 var favicon = require('serve-favicon');
 var rndm = require('rndm');
 var normalizeUrl = require('normalize-url');
+var csrf = require('csurf')
+var crypto = require('crypto');
+
 //==============================
 // APP SETUP
 //==============================
 
 var app = express();
+
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.set('views', __dirname + '/views');
-app.set('port', process.env.PORT || 8080);
 app.use('/static', express.static('public'));
-if(process.env.PRO!=1){
-	app.use(morgan('dev'));
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.set('port', process.env.PORT || 8080);
+
+if(process.env.PRO!=1){
+	app.use(morgan('dev'));
+}
 
 //===============================
 //TEMPLATE
@@ -50,7 +56,6 @@ else{
 	});
 }
 
-
 //===============================
 //DATABASE
 //===============================
@@ -59,7 +64,7 @@ if(process.env.PRO==1){
 	var client = redis.createClient({
 		host:'pub-redis-12002.ap-southeast-1-1.1.ec2.garantiadata.com',
 		port:'12002',
-		pass:process.env.redispassword
+		password:process.env.redispassword
 	})
 }
 else{
@@ -74,12 +79,18 @@ else{
 //===============================
 
 if(process.env.PRO==1){
+	var redisPass = process.env.redispassword;
 	app.use(session({
 		secret: "secrcetodfw4sege34yhsaefd",
 		resave: false,
 		saveUninitialized: false,
 		name:'appSessionId',
-		store: client
+		cookie: { maxAge: 600000 },
+		store: new RedisStore({
+			host:'pub-redis-12002.ap-southeast-1-1.1.ec2.garantiadata.com',
+			port:'12002',
+			pass:redisPass
+		})
 	}));
 }
 else{
@@ -88,7 +99,11 @@ else{
 		resave: false,
 		saveUninitialized: false,
 		name:'appSessionId',
-		store: client
+		cookie: { maxAge: 600000 },
+		store: new RedisStore({
+			host:'localhost',
+			port:'6379'
+		}),
 	}));
 }
 
@@ -96,63 +111,84 @@ else{
 // CUSTOM MIDDLEWARE
 //==================================
 
-app.all('*',(req,res,next)=>{
+app.use(function(req,res,next){
 	res.setHeader("X-Frame-Options", "DENY");
 	res.setHeader("Content-Security-Policy", "frame-ancestors 'none'");
 	res.setHeader("X-powered-by", "none");
 	next();
-})
+});
+
+//================================
+// 
+//================================
+
+var csrfProtection = csrf({cookie:false});
 
 //=====================================================
 // ROUTES
 //=====================================================
 
-app.get('/',function(req,res){
+app.get('/',csrfProtection,function(req,res){
 	if(req.query.invalid&&req.query.invalid==="true"){
-		res.render('home.njk',{message:'Enter a valid url'});
+		res.render('home.njk',{message:'Enter a valid url',csrfToken:req.csrfToken()});
 	}
 	else{
-		res.render('home.njk');
+		res.render('home.njk',{csrfToken:req.csrfToken()});
 	}
 })
 
-app.get('/new',function(req,res){
+app.get('/new',csrfProtection,function(req,res){
 	if(req.query.invalid&&req.query.invalid==="true"){
-		res.render('home.njk',{message:'Enter a valid url'});
+		res.render('home.njk',{message:'Enter a valid url',csrfToken:req.csrfToken()});
 	}
 	else{
-		res.render('home.njk');
+		res.render('home.njk',{csrfToken:req.csrfToken()});
 	}
 })
 
-app.get('/i/:id',function(req,res){
+app.get('/i/:id',csrfProtection,function(req,res){
+	console.log(req.session.attempts);
 	var password = req.query.password || null;
 	var shortUrl = req.params.id;
 	if(shortUrl){
 		client.hgetall(shortUrl,function(err,obj){
 			if(err){
-				res.sendStatus(500);
+				res.sendStatus(401);
 			}
 			else if(obj){
 				if(obj.pwd==="no"){
-					res.sendStatus(403);
+					res.redirect(obj.url);
 				}
 				else{
 					if(password){
 						bcrypt.compare(password, obj.pwd, function(err, result) {
 							if(err){
-								res.sendStatus(403);
+								res.sendStatus(401);
 							}
-							if(result){
-								res.redirect(obj.url);
+							else if(result){
+								var decipher = crypto.createDecipher('AES-192-CTR', password);
+								var decrypted = decipher.update(obj.url, 'hex', 'utf8');
+								decrypted += decipher.final('utf8');
+								delete req.session.attempts;
+								res.redirect(decrypted);
 							}
 							else{
-								res.sendStatus(403);
+								if(!req.session.attempts){
+									req.session.attempts = 1;
+								}
+								req.session.attempts++;	
+								res.render('passwordNeeded.njk',{shortUrl:shortUrl,message:"Wrong Password",csrfToken:req.csrfToken()});
+								//res.sendStatus(403);
 							}
 						});
 					}
 					else{
-						res.sendStatus(403);
+						if(!req.session.attempts){
+							req.session.attempts = 1;
+						}
+						req.session.attempts++;
+						res.render('passwordNeeded.njk',{shortUrl:shortUrl,message:"Password Required",csrfToken:req.csrfToken()});
+						//res.sendStatus(403);
 					}
 				}
 			}
@@ -166,7 +202,19 @@ app.get('/i/:id',function(req,res){
 	}
 });
 
-app.post('/shorten',function(req,res){
+
+app.post('/i/:id',csrfProtection,function(req,res){
+	if(req.session.attempts>5){
+		res.sendStatus(429);
+	}
+	else{
+		res.redirect('/i/' + req.params.id + "?password=" + req.body.password);
+	}
+	
+});
+
+
+app.post('/shorten',csrfProtection,function(req,res){
 	if(req.xhr){
 		res.sendStatus(401);
 	}
@@ -181,23 +229,26 @@ app.post('/shorten',function(req,res){
 		var passwordHash;
 		client.hgetall(shortUrl,function(err,obj){
 			if(err){					
-				res.sendStatus(500);
+				res.sendStatus(401);
 			}
 			else{
 				if(obj){
 					shortUrl+=rndm.base62(3);
 				}
 				if(password!="no"){
+					var cipher = crypto.createCipher('AES-192-CTR', password);
+					var urlHash = cipher.update(url, 'utf8', 'hex');
+					urlHash += cipher.final('hex'); 
 					bcrypt.genSalt(10, function(err, salt) {
 						bcrypt.hash(password, salt, function(err, hash) {
-							client.hmset(shortUrl, "url", url , "pwd", hash , function(err,reply){
+							client.hmset(shortUrl, "url", urlHash , "pwd", hash , function(err,reply){
 								if(err){
 									//console.log(err);
 									res.sendStatus(401);
 								}
 								else{
 									if(reply==="OK"){
-										res.render('created.njk',{shortUrl:req.hostname+"/i/"+shortUrl+"?password=" + password});
+										res.render('created.njk',{shortUrl:req.hostname+"/i/"+shortUrl,password:password});
 									}
 									else{
 										res.sendStatus(401);
@@ -211,19 +262,19 @@ app.post('/shorten',function(req,res){
 					client.hmset(shortUrl, "url", url , "pwd", "no" , function(err,reply){
 						if(err){
 							//console.log(err);
-							res.sendStatus(500);
+							res.sendStatus(401);
 						}
 						else{
 							if(reply==="OK"){
-								res.render('created.njk',{shortUrl:req.hostname+"/i/"+shortUrl});
+								res.render('created.njk',{shortUrl:req.hostname+"/i/"+shortUrl,password:"no"});
 							}
 							else{
-								res.sendStatus(500);
+								res.sendStatus(401);
 							}
 						}
 					});
 				}
-			}
+			}		
 		});
 	}
 })
@@ -231,6 +282,20 @@ app.post('/shorten',function(req,res){
 app.get('/shorten',function(req,res){
 	res.redirect('/');
 });
+
+app.use(function(err,req,res,next){
+	if(!err){
+		next();
+	}
+	else if (err.code !== 'EBADCSRFTOKEN') {
+		next(err)
+	}
+	else{
+		res.status(403);
+		res.send("Form Tampered With");
+	}
+	
+})
 
 app.use(function(req, res, next) {
     res.status(404);
